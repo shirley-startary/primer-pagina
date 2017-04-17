eb02eed27846c2f303e99c2e2c997278d5648844
Shirley Silvana Suarez Startary <Shirley Silvana Suarez Startary>
2017-04-17 12:10:16 -0500

    corrigiendo el codigo, para que no cree dinamicamente, y creando el footer.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,13 @@
 var boxWork=document.getElementsByClassName("imgProducto");
-//console.log(boxWork);
+console.log(boxWork);
 
 for (var i=0;i<boxWork.length;i++){
-  boxWork[i].addEventListener("click",function(){abrir(this)});
+  boxWork[i].addEventListener("click",otro);
 
+}
+
+function otro(){
+  abrir(this)
 }
 //console.log(boxWork);
 function abrir(imagenProducto){
@@ -12,31 +16,34 @@ function abrir(imagenProducto){
     var elemento=imagenProducto.src;
     //console.log(elemento);
     var imagen=crearModalImagen(elemento);
-    console.log(imagen);
+    //console.log(imagen);
     mostrarModal(imagen);
 }
 
 function crearModalImagen(imgsrc){
-  var fondo=document.createElement("div");
-  var modal=document.createElement("div");
-  var imagen=document.createElement("img");
+  //var fondo=document.createElement("div");
+  var fondo=document.getElementById("fondo");
+  //console.log(fondo);
+  var modal=document.getElementById("modal");
+  var imagen=document.getElementById("_imagen");
   imagen.src=imgsrc;
   imagen.style.width="90%";
 
-  fondo.appendChild(modal);
-  modal.appendChild(imagen);
+  //fondo.appendChild(modal);
+  //modal.appendChild(imagen);
   fondo.className="imageProduct";
   modal.className="modal";
 
-  var cerrar=document.createElement("div");
-  var x=document.createTextNode("X");
-  cerrar.appendChild(x);
+  var cerrar=document.getElementById("cerrar");
+  //var x=document.createTextNode("X");
+  //cerrar.appendChild(x);
+  cerrar.innerText="X";
   cerrar.className="cerrar";
   cerrar.addEventListener("click",function(){
   fondo.style.visibility="hidden";
   });
-  modal.appendChild(cerrar);
-  document.body.appendChild(fondo);
+  //modal.appendChild(cerrar);
+  //document.body.appendChild(fondo);
   return fondo;
 }
 
