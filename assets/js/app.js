d98d86795dd4c1eb927a831a8e4a86b96f62c89b
Shirley Silvana Suarez Startary <Shirley Silvana Suarez Startary>
2017-04-17 00:49:02 -0500

    creando funciones para crea y mostrar el modal, se descarta efectos, y crea los estilos para el modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,45 @@
-var boxWork=document.getElementsByClassName("box-work");
-console.log(boxWork);
-
+var boxWork=document.getElementsByClassName("imgProducto");
+//console.log(boxWork);
 
 for (var i=0;i<boxWork.length;i++){
-  boxWork[i]=addEventListener("click",abrir);
+  boxWork[i].addEventListener("click",function(){abrir(this)});
+
+}
+//console.log(boxWork);
+function abrir(imagenProducto){
+  // alert("hola");
+    //console.log(imagenProducto.src);
+    var elemento=imagenProducto.src;
+    //console.log(elemento);
+    var imagen=crearModalImagen(elemento);
+    console.log(imagen);
+    mostrarModal(imagen);
 }
 
-function abrir(){
-  // alert("hola");
+function crearModalImagen(imgsrc){
+  var fondo=document.createElement("div");
+  var modal=document.createElement("div");
+  var imagen=document.createElement("img");
+  imagen.src=imgsrc;
+  imagen.style.width="90%";
+
+  fondo.appendChild(modal);
+  modal.appendChild(imagen);
+  fondo.className="imageProduct";
+  modal.className="modal";
+
+  var cerrar=document.createElement("div");
+  var x=document.createTextNode("X");
+  cerrar.appendChild(x);
+  cerrar.className="cerrar";
+  cerrar.addEventListener("click",function(){
+  fondo.style.visibility="hidden";
+  });
+  modal.appendChild(cerrar);
+  document.body.appendChild(fondo);
+  return fondo;
+}
+
+function mostrarModal(obj){
+  obj.style.visibility="visible";
 }
